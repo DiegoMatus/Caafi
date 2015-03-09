8a2372813437e599e7d1a99a3b8b07d3930ccc69
Diego Matus <badboy_immm@hotmail.com>
2015-03-08 22:17:43 -0600

    Primeras URL's y views añadidas

--- sistema/wsgi.py
+++ sistema/wsgi.py
@@ -7,13 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-
-"""import os
+import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()"""
+application = get_wsgi_application()
